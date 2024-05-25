f14259d2c93097c5307d97534ed2a9c9c0f01b1a
ama-chi <89921925+ama-chi@users.noreply.github.com>
2024-05-25 17:52:31 +0800

    Frontend update

--- frontend-hacksg/src/components/NavBar.tsx
+++ frontend-hacksg/src/components/NavBar.tsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useNavigate } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
-
 import { useAuthContext } from '../hooks/useAuthContext';
 
-const NavBar = () => {
+import '../App.css'; // Make sure this imports your styles
 
+const NavBar = () => {
     const { logout } = useLogout();
     const navigate = useNavigate();
 
@@ -19,49 +20,25 @@ const NavBar = () => {
         }
     };
 
-  const { user } = useAuthContext();
+    const { user } = useAuthContext();
 
-  console.log(user)
+    console.log(user);
 
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-        { user && user.user.role == "volunteer" ? (
+    return (
+        <Navbar expand="lg" className="navbar-custom">
             <Container>
                 <Navbar.Brand href="/">Pango</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="/profile/volunteer">Profile</Nav.Link>
-                    <Nav.Link href="/events/volunteer">Events</Nav.Link>
-                    <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                    <Nav.Link href="/profile/volunteer" className="nav-link-custom">Profile</Nav.Link>
+                    <Nav.Link href="/events/volunteer" className="nav-link-custom">Events</Nav.Link>
+                    <Nav.Link onClick={handleLogout} className="nav-link-custom">Logout</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
-        ) : user && user.user.role == "organisation" ? (
-            <Container>
-                <Navbar.Brand href="/">Pango</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/profile/org">Profile</Nav.Link>
-                    <Nav.Link href="/events/org">Events</Nav.Link>
-                    <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        ) : (
-            <Container>
-                <Navbar.Brand href="/">Pango</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/selectUserType">Login/Signup</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        )}
-    </Navbar>
-  );
+        </Navbar>
+    );
 }
 
 export default NavBar;
--- frontend-hacksg/src/pages/ViewEvents.tsx
+++ frontend-hacksg/src/pages/ViewEvents.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBContainer } from 'mdb-react-ui-kit';
+import { paste } from '@testing-library/user-event/dist/paste';
 
 // Define the type for an event
 interface Event {
@@ -11,7 +12,7 @@ interface Event {
 }
 
 // Mock data
-const mockEvents: Event[] = [
+const mockPastEvents: Event[] = [
     { id: 1, name: "Beach Cleanup", location: "East Coast Park", date: "2024-06-25", participants: 150 },
     { id: 2, name: "Tree Planting Day", location: "Bishan Park", date: "2024-07-01", participants: 80 },
     { id: 3, name: "Charity Run", location: "Marina Bay", date: "2024-07-15", participants: 200 },
@@ -20,44 +21,71 @@ const mockEvents: Event[] = [
     { id: 6, name: "Historical Tour Guiding", location: "Fort Canning Park", date: "2024-08-12", participants: 75 },
     { id: 7, name: "Local Library Organization", location: "Queenstown Library", date: "2024-08-18", participants: 50 },
     { id: 8, name: "River Cleanup", location: "Singapore River", date: "2024-09-01", participants: 200 },
-    { id: 9, name: "Food Bank Assistance", location: "Woodlands", date: "2024-09-10", participants: 65 },
-    { id: 10, name: "Elderly Home Visit", location: "Yishun", date: "2024-09-20", participants: 40 },
-    { id: 11, name: "Beach Yoga Event", location: "Sentosa Beach", date: "2024-10-05", participants: 100 },
-    { id: 12, name: "Heritage Tree Planting", location: "Botanic Gardens", date: "2024-10-12", participants: 150 },
-    { id: 13, name: "Coastal Watch", location: "Changi Beach", date: "2024-10-22", participants: 110 },
-    { id: 14, name: "Fitness for a Cause", location: "Bukit Timah Nature Reserve", date: "2024-11-02", participants: 130 },
-    { id: 15, name: "Charity Concert", location: "Esplanade", date: "2024-11-15", participants: 300 },
-    { id: 16, name: "Neighborhood Clean-up", location: "Hougang", date: "2024-12-05", participants: 85 },
-    { id: 17, name: "Festival Volunteering", location: "Clarke Quay", date: "2024-12-20", participants: 250 },
-    { id: 18, name: "New Year Event Preparation", location: "Marina Bay Sands", date: "2024-12-30", participants: 500 }
+
   ];
+
+const mockUpcomingEvents: Event[] = [
+  { id: 1, name: "Food Bank Assistance", location: "Woodlands", date: "2024-09-10", participants: 65 },
+  { id: 2, name: "Elderly Home Visit", location: "Yishun", date: "2024-09-20", participants: 40 },
+  { id: 3, name: "Beach Yoga Event", location: "Sentosa Beach", date: "2024-10-05", participants: 100 },
+  { id: 4, name: "Heritage Tree Planting", location: "Botanic Gardens", date: "2024-10-12", participants: 150 },
+  { id: 5, name: "Coastal Watch", location: "Changi Beach", date: "2024-10-22", participants: 110 },
+  { id: 6, name: "Fitness for a Cause", location: "Bukit Timah Nature Reserve", date: "2024-11-02", participants: 130 },
+  { id: 7, name: "Charity Concert", location: "Esplanade", date: "2024-11-15", participants: 300 },
+  { id: 8, name: "Neighborhood Clean-up", location: "Hougang", date: "2024-12-05", participants: 85 },
+  { id: 9, name: "Festival Volunteering", location: "Clarke Quay", date: "2024-12-20", participants: 250 },
+  { id: 10, name: "New Year Event Preparation", location: "Marina Bay Sands", date: "2024-12-30", participants: 500 }
+
+];
   
 const ViewEvents: React.FC = () => {
-  const [events, setEvents] = useState<Event[]>([]);
+  const [pastevents, setPastEvents] = useState<Event[]>([]);
+  const [futureevents, setFutureEvents] = useState<Event[]>([]);
 
   useEffect(() => {
     // Simulate fetching data from an API
-    setEvents(mockEvents);
+    setPastEvents(mockPastEvents);
+    setFutureEvents(mockUpcomingEvents);
   }, []);
 
   return (
-    <MDBContainer className="my-5">
-      <h2 className="mb-4">Upcoming Events</h2>
-      {events.map((event) => (
-        <MDBCard key={event.id} className="mb-3">
-          <MDBCardBody>
-            <MDBCardTitle>{event.name}</MDBCardTitle>
-            <MDBCardText>
-              Location: {event.location}
-              <br />
-              Date: {event.date}
-              <br />
-              Participants: {event.participants}
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      ))}
-    </MDBContainer>
+    <>
+      <MDBContainer className="my-5" >
+        <h2 className="mb-4">Upcoming Events</h2>
+        {futureevents.map((event) => (
+          <MDBCard key={event.id} className="mb-3">
+            <MDBCardBody>
+              <MDBCardTitle>{event.name}</MDBCardTitle>
+              <MDBCardText>
+                Location: {event.location}
+                <br />
+                Date: {event.date}
+                <br />
+                Participants: {event.participants}
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        ))}
+      </MDBContainer>
+
+      <MDBContainer className="my-5">
+        <h2 className="mb-4">Completed Events</h2>
+        {pastevents.map((event) => (
+          <MDBCard key={event.id} className="mb-3">
+            <MDBCardBody>
+              <MDBCardTitle>{event.name}</MDBCardTitle>
+              <MDBCardText>
+                Location: {event.location}
+                <br />
+                Date: {event.date}
+                <br />
+                Participants: {event.participants}
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        ))}
+      </MDBContainer>
+    </>
   );
 }
 
