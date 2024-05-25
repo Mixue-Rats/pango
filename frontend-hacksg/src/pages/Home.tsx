627d637bc25134242a3b674ffb7614be4b6996e4
ama-chi <89921925+ama-chi@users.noreply.github.com>
2024-05-25 16:01:07 +0800

    Add Home page

--- frontend-hacksg/src/pages/Home.tsx
+++ frontend-hacksg/src/pages/Home.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
-import SetPreferences from './forms/SetPreferences';
 
-// import volunteer from '../assets/images/volunteer-1.png';
-import banner from '../assets/images/banner.png';
-
-let volunteer = "https://via.placeholder.com/500"
+// Importing the image from local assets directory
+import image from './assets/images/test.png';  // Adjust the path as necessary
 
 const Home = () => {
     return (
-        <div className='page'>
-            {/* <Container className='min-vw-100' style={{backgroundImage: `url(${volunteer})`, backgroundRepeat: 'no-repeat', border: '2px solid red'}}>
+        <div className='page' style={{ backgroundColor: '#88AED0' }}> 
+            <h1 className='text-center mt-3'>Welcome back!</h1>
+            <Container className='d-flex justify-content-center align-items-center' style={{ minHeight: '100vh' }}>
+            
                 <Row>
-                    <h1 className='text-center'>Home</h1>
+                    <Col>
+                        {/* Inline styles to set image size */}
+                        <Image src={image} alt="Banner" style={{ width: '600px', height: 'auto' }} className="mx-auto d-block" />
+                        <h1 className='text-center mt-3'>Ready to help?</h1>
+                    </Col>
                 </Row>
-            </Container> */}
-            <SetPreferences/>
+            </Container>
         </div>
     );
 }
